a4c8dbe26cb8e0e010b9796d0e376e5529d6bd27
ds-rohan-dev <rohan@digi-safari.com>
2025-07-10 11:07:45 -0400

    Updated to day 3 and 4

--- Day2/start/services/bank-service/src/routes/add-bank-route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const addbank = require("../controllers/add-bank-controller");
-
-const router = express.Router();
-
-router.post("/api/banks/addbank", addbank);
-
-module.exports = { addBankRouter: router };
